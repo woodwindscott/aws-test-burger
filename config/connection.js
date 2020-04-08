78b395e8971b755dd2d4c898e138e770bf858350
Scott Moore <woodwindscott@yahoo.com>
2020-04-07 21:31:04 -0400

    database connection configuration through AWS RDS

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "zq6hqqgieuhazqex",
-  password: "zhvpqechwxfwczdk",
-  database: "jda9x20ca52drx4z"
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.RDS_PORT,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB_NAME
 });
 
 // Make connection.
