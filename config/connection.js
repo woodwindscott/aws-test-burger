cf9035ff366088f7a743ef1a567f4c97abdc3c50
Scott Moore <woodwindscott@yahoo.com>
2020-04-08 19:59:13 -0400

    New database connection details

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME,
-  port: process.env.RDS_PORT,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DB_NAME
-});
-
 // var connection = mysql.createConnection({
-//   host: "new-practice-database.c8mftva3qql4.us-east-1.rds.amazonaws.com",
-//   port: 3306,
-//   user: "masterUsername",
-//   password: "Care1self!",
-//   database: "burgers"
+//   host: process.env.RDS_HOSTNAME,
+//   port: process.env.RDS_PORT,
+//   user: process.env.RDS_USERNAME,
+//   password: process.env.RDS_PASSWORD,
+//   database: process.env.RDS_DB_NAME
 // });
+
+var connection = mysql.createConnection({
+  host: "aa18dsw4r9wyv28.c8mftva3qql4.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "admin",
+  password: "Care1self!",
+  database: "burgers"
+});
 
 // var connection = mysql.createConnection({
 //   host: process.env.MY_HOSTNAME,
