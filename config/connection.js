73932517146d436704bcc21167e729ffcd6ecdb2
Scott Moore <woodwindscott@yahoo.com>
2020-04-07 21:48:27 -0400

    Update database connection

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,11 @@ var mysql = require("mysql");
 // });
 
 var connection = mysql.createConnection({
-  host: "sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "aa1nebb9sip7z5v.c8mftva3qql4.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "zq6hqqgieuhazqex",
-  password: "zhvpqechwxfwczdk",
-  database: "jda9x20ca52drx4z"
+  user: "root",
+  password: "Care1self!",
+  database: "aa1nebb9sip7z5v"
 });
 
 
