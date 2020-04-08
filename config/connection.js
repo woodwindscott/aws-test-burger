ff9018ad25a756b0a0df2308782ef7ccf3737059
Scott Moore <woodwindscott@yahoo.com>
2020-04-07 21:42:27 -0400

    Updates to connection file

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: process.env.RDS_HOSTNAME,
+//   port: process.env.RDS_PORT,
+//   user: process.env.RDS_USERNAME,
+//   password: process.env.RDS_PASSWORD,
+//   database: process.env.RDS_DB_NAME
+// });
+
 var connection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME,
-  port: process.env.RDS_PORT,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DB_NAME
+  host: "sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "zq6hqqgieuhazqex",
+  password: "zhvpqechwxfwczdk",
+  database: "jda9x20ca52drx4z"
 });
+
 
 // Make connection.
 connection.connect(function(err) {
