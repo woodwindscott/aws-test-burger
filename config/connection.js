f30de1f10c504cf4be4db5ace907daf5a900fc71
Scott Moore <woodwindscott@yahoo.com>
2020-04-08 19:03:54 -0400

    updates again

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: process.env.RDS_HOSTNAME,
-//   port: process.env.RDS_PORT,
-//   user: process.env.RDS_USERNAME,
-//   password: process.env.RDS_PASSWORD,
-//   database: process.env.RDS_DB_NAME
-// });
+var connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  port: process.env.RDS_PORT,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB_NAME
+});
 
 // var connection = mysql.createConnection({
 //   host: "new-practice-database.c8mftva3qql4.us-east-1.rds.amazonaws.com",
@@ -17,13 +17,13 @@ var mysql = require("mysql");
 //   database: "burgers"
 // });
 
-var connection = mysql.createConnection({
-  host: process.env.MY_HOSTNAME,
-  port: 3306,
-  user: "zq6hqqgieuhazqex",
-  password: "zhvpqechwxfwczdk",
-  database: "jda9x20ca52drx4z"
-});
+// var connection = mysql.createConnection({
+//   host: process.env.MY_HOSTNAME,
+//   port: 3306,
+//   user: "zq6hqqgieuhazqex",
+//   password: "zhvpqechwxfwczdk",
+//   database: "jda9x20ca52drx4z"
+// });
 
 
 // Make connection.
